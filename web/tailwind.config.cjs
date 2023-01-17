eae0854ec5d338a0b67b07fa01a3df267447f36d
pauloWeslleY <weslleyspfc@13outlook.com>
2023-01-17 16:26:38 -0300

    AULA 01: Mobile finalizado

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.tsx', './index.html'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
