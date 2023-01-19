1338412eecb98079e0530d9781d3609fb1f660fd
pauloWeslleY <weslleyspfc@13outlook.com>
2023-01-19 03:22:21 -0300

    AULA03 - min52:35

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,19 +1,32 @@
 import { Plus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import LogoImage from "../assets/logo.svg";
 
 export function Header() {
+   /*
+      [] Imperativa vs. Declarativa
+      [] JS/HTML
+   */
    return (
       <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
          <img src={LogoImage} alt="Habits" />
 
-         <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-         >
-            <Plus size={20} className="text-violet-500" />
-            Novo hábito
-         </button>
+         <Dialog.Root>
+            <Dialog.Trigger
+               type="button"
+               className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+            >
+               <Plus size={20} className="text-violet-500" />
+               Novo hábito
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+               <Dialog.Overlay />
+
+               <Dialog.Content>Conteúdo do Modal</Dialog.Content>
+            </Dialog.Portal>
+         </Dialog.Root>
       </div>
    );
 }
