24655a6826c5014b60b264fd1304e2466e072b25
pauloWeslleY <weslleyspfc@13outlook.com>
2023-01-19 21:15:21 -0300

    AULA04 - Conexão com Front-End finalizada min58:42

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,8 +1,57 @@
+import { FormEvent, useState } from "react";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { api } from "../lib/axios";
+
+const availableWeekDays = [
+   "Domingo",
+   "Segunda-feira",
+   "Terça-feria",
+   "Quarta-feria",
+   "Quinta-feira",
+   "Sexta-feira",
+   "Sábado",
+];
 
 export function NewHabitForm() {
+   const [title, setTitle] = useState("");
+   const [weekDays, setWeekDays] = useState<number[]>([]);
+
+   async function createNewHabit(event: FormEvent) {
+      event.preventDefault();
+
+      console.log(title, weekDays);
+
+      if (!title || weekDays.length === 0) {
+         return;
+      }
+
+      await api.post("habits", {
+         title,
+         weekDays,
+      });
+
+      setTitle("");
+      setWeekDays([]);
+
+      alert("Hábito criado com sucesso!");
+   }
+
+   function handleToggleWeekDay(weekDay: number) {
+      if (weekDays.includes(weekDay)) {
+         const weekDayWithRemovedOne = weekDays.filter(
+            (day) => day !== weekDay
+         );
+         setWeekDays(weekDayWithRemovedOne);
+      } else {
+         const weekDaysWithAddedOne = [...weekDays, weekDay];
+
+         setWeekDays(weekDaysWithAddedOne);
+      }
+   }
+
    return (
-      <form className="w-full flex flex-col mt-6">
+      <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
          <label htmlFor="title" className="font-semibold leading-tight">
             Qual seu comprometimento?
          </label>
@@ -13,11 +62,34 @@ export function NewHabitForm() {
             placeholder="ex.: Exercícios, dormir bem, etc..."
             className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
             autoFocus
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
          />
 
          <label htmlFor="" className="font-semibold leading-tight mt-4">
             Qual a recorrência?
          </label>
+
+         <div className="flex flex-col gap-3 mt-3">
+            {availableWeekDays.map((weekDay, index) => (
+               <Checkbox.Root
+                  key={weekDay}
+                  className="flex items-center gap-3 group"
+                  onCheckedChange={() => handleToggleWeekDay(index)}
+                  checked={weekDays.includes(index)}
+               >
+                  <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                     <Checkbox.Indicator>
+                        <Check size={20} className="text-white" />
+                     </Checkbox.Indicator>
+                  </div>
+
+                  <span className="text-white leading-tight font-semibold">
+                     {weekDay}
+                  </span>
+               </Checkbox.Root>
+            ))}
+         </div>
 
          <button
             type="submit"
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,9 @@
 import { HabitDay } from "./HabitDay";
 
 import { generateDateFromYearBeginning } from "../utils/generate-date-from-year-beginning";
+import { api } from "../lib/axios";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 const weekDay = ["D", "S", "T", "Q", "Q", "S", "S"];
 
@@ -9,7 +12,22 @@ const summaryDates = generateDateFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7; //! 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type SummaryProps = Array<{
+   id: string;
+   date: string;
+   amount: number;
+   completed: number;
+}>;
+
 export function SummaryTable() {
+   const [summary, setSummary] = useState<SummaryProps>([]);
+
+   useEffect(() => {
+      api.get("/summary").then((response) => {
+         setSummary(response.data);
+      });
+   }, []);
+
    return (
       <div className="w-full flex">
          <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -27,11 +45,16 @@ export function SummaryTable() {
 
          <div className="grid grid-rows-7 grid-flow-col gap-3">
             {summaryDates.map((date) => {
+               const dayInSummary = summary.find((day) => {
+                  return dayjs(date).isSame(day.date, "day");
+               });
+
                return (
                   <HabitDay
                      key={date.toString()}
-                     amount={5}
-                     completed={Math.round(Math.random() * 5)}
+                     date={date}
+                     amount={dayInSummary?.amount}
+                     completed={dayInSummary?.completed}
                   />
                );
             })}
